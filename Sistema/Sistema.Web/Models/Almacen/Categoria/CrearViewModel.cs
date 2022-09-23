339233cb7fb58695b1049790f23e37f8291d05e8
tito <tito2112230@gmail.com>
2022-09-23 06:43:09 -0500

    primera commit repo de ventas 23-09-2022

---
+++ Sistema/Sistema.Web/Models/Almacen/Categoria/CrearViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Web.Models.Almacen.Categoria
+{
+    public class CrearViewModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe de tener mas de 50 caracteres, ni menos de 3 caracteres.")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+    }
+}
